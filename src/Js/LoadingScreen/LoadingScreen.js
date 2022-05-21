9507899579b648379b9fd422392cdcb35757e67c
ThePiteroXx <thepremiumxx@wp.pl>
2022-05-21 16:44:33 +0200

    improved animations

--- src/Js/LoadingScreen/LoadingScreen.js
+++ src/Js/LoadingScreen/LoadingScreen.js
@@ -38,33 +38,53 @@ export default class LoadingScreen
         const headers = [...document.querySelectorAll('[data-heading]')]
         const descriptions = [...document.querySelectorAll('[data-description]')]
         const canvas = [...document.querySelectorAll('[data-canvas]')]
-        let descriptionImg;
         const descriptionHome = document.querySelector('.home__description')
         const btnHome = document.querySelector('.home__btn')
+        let descriptionImg
 
         //animation headers
-        let spansHeader = []
+        const spansHeader = []
         headers.forEach(header => spansHeader.push(header.children))
-        spansHeader.forEach(span => {
-           this.tl.fromTo(span, {opacity:0}, {opacity:1, stagger: 0.05}, 'headersAnimation')
+        spansHeader.forEach(header => {
+           this.tl.fromTo(header, {opacity:0}, {opacity:1, stagger: 0.05}, 'headersAnimation')
         })
-       this.tl.addLabel('afterHeaders')
+        this.tl.addLabel('afterHeaders')
 
-       //animation description home and btn
-       let spansDescriptionHome = descriptionHome.children
-       this.tl.fromTo(spansDescriptionHome, {opacity:0}, {opacity:1, stagger: 0.04})
-       this.tl.fromTo(btnHome, {opacity: 0}, {opacity:1, duration: 0.5})
+        //animation description home and btn
+        let spansDescriptionHome = descriptionHome.children
+        this.tl.fromTo(spansDescriptionHome, {opacity:0}, {opacity:1, stagger: 0.04})
+        this.tl.fromTo(btnHome, {opacity: 0}, {opacity:1, duration: 0.5})
 
-       //animation all canvas and descriptions without home
-       this.tl.fromTo(descriptions, {opacity:0}, {opacity:1, stagger: 0.3}, 'afterHeaders')
-       this.tl.fromTo(canvas, {opacity:0}, {opacity:1, stagger: 0.01}, 'afterHeaders')
+        //animation all canvas and descriptions without home
+        const sortedDesciptions = this.sortArrayOnAttribute(descriptions, 'data-description')
+        sortedDesciptions.forEach(el => {
+            this.tl.fromTo(el, {opacity:0}, {opacity:1, stagger: 0.3}, 'afterHeaders')
+        })
 
-       //animation img work
-       .add(() => {
-           descriptionImg = [...document.querySelectorAll('.work__container__img-set')]
-           this.tl.fromTo(descriptionImg, {opacity:0}, {opacity:1, stagger: 0.2}, 'afterHeaders')
-       }, 'begin')
+        this.tl.fromTo(canvas, {opacity:0}, {opacity:1, duration: 1}, 'afterHeaders')
+
+        //animation img work
+        .add(() => {
+                descriptionImg = [...document.querySelectorAll('.work__container__img-set')]
+                this.tl.fromTo(descriptionImg, {opacity:0}, {opacity:1, stagger: 0.2}, 'afterHeaders')
+        }, 'begin')
        
+    }
+
+    sortArrayOnAttribute(array, nameOfAttribute) {
+        const sortedArray = []
+        const helpfulArray = []
+        array.forEach((el, index, array) => {
+            helpfulArray.push(el)
+
+            if(el.attributes[nameOfAttribute].value !== array[index + 1]?.attributes[nameOfAttribute].value)
+            {
+               sortedArray.push([...helpfulArray])
+               helpfulArray.length = 0
+            }
+        })
+
+        return sortedArray
     }
 
 }
